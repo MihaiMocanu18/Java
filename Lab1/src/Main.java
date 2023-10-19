65c503255b6de482a7ca6d4987a72a41996f6a84
MihaiMocanu18 <mihai.mocanu89@gmail.com>
2023-10-19 16:27:11 +0300

    first commit

---
+++ Lab1/src/Main.java
@@ -0,0 +1,11 @@
+class Vars {
+    public static void main(Sting args[]) {
+        int x = 150;
+                double y = 2.0;
+                float z = 20f;
+                byte b = 10;
+                short c = 14;
+                y  = x;
+                System.out.println(y);
+    }
+}
---
+++ Laborator 1/Laboratorul 1/src/MonthDays.java
@@ -0,0 +1,10 @@
+public class MonthDays {
+    public static void main(String[] args) {
+        int month_days[] = {31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
+        String months[] = {"Ianuarie", "Februarie", "Martie", "Aprilie", "Mai", "Iunie",
+                "Iulie", "August", "Septembrie", "Octombrie", "Noiembrie", "Decembrie"};
+        for (int i = 0; i < months.length; i++) {
+            System.out.println("Luna " + months[i] + " Ziua " + month_days[i] );
+        }
+    }
+}
